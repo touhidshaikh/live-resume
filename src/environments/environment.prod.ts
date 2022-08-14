30c011a63857423d47b5eb09f38fac4423a1c083
Touhid M Shaikh <touhidshaikh22@gmail.com>
2022-08-14 06:23:30 +0530

    first

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,22 +2,22 @@ export const environment = {
   production: true,
   // replace the data below with your personal data
   personal: {
-    caricature: "gbastos-illustration@2x-mask.png",
-    name: "Guilherme Borges Bastos",
-    birth: "1993-06-29",
-    email: "guilhermeborgesbastos@gmail.com",
-    phone: "+31 68 733 3020",
+    caricature: "tms-illustration@2x-mask.png",
+    name: "Touhid M Shaikh",
+    birth: "1993-02-22",
+    email: "tms@touhidshaikh.com",
+    phone: "+91 7738794435",
     location: "Rotterdam, South Holland, Netherlands"
   },
   // replace the dummy data below with the real firebase configs
-  firebaseConfig: {
-    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
-    authDomain: "live-resume-a575a.firebaseapp.com",
-    databaseURL: "https://live-resume-a575a.firebaseio.com",
-    projectId: "live-resume-a575a",
-    storageBucket: "live-resume-a575a.appspot.com",
-    messagingSenderId: "681076751855",
-    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-    measurementId: "G-00VXD77WNG"
-  }
+//  firebaseConfig: {
+//    apiKey: "a",
+//    authDomain: "live-a-a575a.firebaseapp.com",
+//    databaseURL: "https://s-resume-a575a.firebaseio.com",
+//    projectId: "live-resume-a575a",
+//    storageBucket: "live-resume-a575a.appspot.com",
+//    messagingSenderId: "681076751855",
+//    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+//    measurementId: "G-00VXD77WNG"
+//  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,24 +6,24 @@ export const environment = {
   production: false,
   // replace the data below with your personal data
   personal: {
-    caricature: "gbastos-illustration@2x-mask.png",
-    name: "Guilherme Borges Bastos",
-    birth: "1993-06-29",
-    email: "guilhermeborgesbastos@gmail.com",
-    phone: "+31 68 733 3020",
+    caricature: "tms-illustration@2x-mask.png",
+    name: "Touhid M Shaikh",
+    birth: "1993-02-22",
+    email: "tms@touhidshaikh.com",
+    phone: "+91 7738794435",
     location: "Rotterdam, South Holland, Netherlands"
   },
   // replace the dummy data below with the real firebase configs
-  firebaseConfig: {
-    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
-    authDomain: "live-resume-a575a.firebaseapp.com",
-    databaseURL: "https://live-resume-a575a.firebaseio.com",
-    projectId: "live-resume-a575a",
-    storageBucket: "live-resume-a575a.appspot.com",
-    messagingSenderId: "681076751855",
-    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-    measurementId: "G-00VXD77WNG"
-  }
+//  firebaseConfig: {
+//    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+//    authDomain: "live-resume-a575a.firebaseapp.com",
+//    databaseURL: "https://live-resume-a575a.firebaseio.com",
+//    projectId: "live-resume-a575a",
+//    storageBucket: "live-resume-a575a.appspot.com",
+//    messagingSenderId: "681076751855",
+//    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+//    measurementId: "G-00VXD77WNG"
+//  }
 };
 
 /*
