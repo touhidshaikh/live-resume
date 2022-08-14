3ea4eb5b4367f689d2adba20a981279d643ab170
Touhid M Shaikh <touhidshaikh22@gmail.com>
2022-08-14 06:25:46 +0530

    firebase update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,14 +10,14 @@ export const environment = {
     location: "Rotterdam, South Holland, Netherlands"
   },
   // replace the dummy data below with the real firebase configs
-//  firebaseConfig: {
-//    apiKey: "a",
-//    authDomain: "live-a-a575a.firebaseapp.com",
-//    databaseURL: "https://s-resume-a575a.firebaseio.com",
-//    projectId: "live-resume-a575a",
-//    storageBucket: "live-resume-a575a.appspot.com",
-//    messagingSenderId: "681076751855",
-//    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-//    measurementId: "G-00VXD77WNG"
-//  }
+  firebaseConfig: {
+    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+    authDomain: "live-resume-a575a.firebaseapp.com",
+    databaseURL: "https://live-resume-a575a.firebaseio.com",
+    projectId: "live-resume-a575a",
+    storageBucket: "live-resume-a575a.appspot.com",
+    messagingSenderId: "681076751855",
+    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+    measurementId: "G-00VXD77WNG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,16 +14,16 @@ export const environment = {
     location: "Rotterdam, South Holland, Netherlands"
   },
   // replace the dummy data below with the real firebase configs
-//  firebaseConfig: {
-//    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
-//    authDomain: "live-resume-a575a.firebaseapp.com",
-//    databaseURL: "https://live-resume-a575a.firebaseio.com",
-//    projectId: "live-resume-a575a",
-//    storageBucket: "live-resume-a575a.appspot.com",
-//    messagingSenderId: "681076751855",
-//    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-//    measurementId: "G-00VXD77WNG"
-//  }
+  firebaseConfig: {
+    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+    authDomain: "live-resume-a575a.firebaseapp.com",
+    databaseURL: "https://live-resume-a575a.firebaseio.com",
+    projectId: "live-resume-a575a",
+    storageBucket: "live-resume-a575a.appspot.com",
+    messagingSenderId: "681076751855",
+    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+    measurementId: "G-00VXD77WNG"
+  }
 };
 
 /*
